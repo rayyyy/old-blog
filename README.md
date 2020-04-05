6c594685d14ff2a0f71bdfeced58914aca96862e
rayyyy <nraavy@gmail.com>
2020-04-06 01:14:01 +0900

    article

--- README.md
+++ README.md
@@ -37,11 +37,9 @@ SEO的なところ
 [プラグイン45選](https://qiita.com/Takumon/items/da8347f81a9f021b637f#gatsby-plugin-robots-txt)
 
 ## やること 優先度高い順
-* tagを水色にする
-* 記事一覧のパーツをarticleタグで囲む
+* tagフィルターページ
 * RSS feed
 * 時間をUTCからJSTにする
-* tagフィルターページ
 
 
 ## やってみたいところ
--- src/components/molecules/post-link/post-link.js
+++ src/components/molecules/post-link/post-link.js
@@ -4,29 +4,31 @@ import styles from './post-link.module.sass'
 import Tags from '../tags/tags'
 
 const PostLink = ({ post }) => (
-  <div className={styles.card}>
-    <Link to={post.fields.path}>
-      <div className={styles.header}>
+  <article>
+    <div className={styles.card}>
+      <Link to={post.fields.path}>
+        <div className={styles.header}>
+          <div className={styles.title}>
+            {post.frontmatter.title}
+          </div>
+          <div className={styles.date}>
+            {post.frontmatter.updated_date}
+          </div>
+        </div>
+        <div className={styles.main}>
+          {post.excerpt}
+        </div>
+      </Link>
+      <div className={styles.footer}>
         <div className={styles.title}>
-          {post.frontmatter.title}
+          タグ
         </div>
-        <div className={styles.date}>
-          {post.frontmatter.updated_date}
+        <div>
+          <Tags tags={post.frontmatter.tags} />
         </div>
-      </div>
-      <div className={styles.main}>
-        {post.excerpt}
-      </div>
-    </Link>
-    <div className={styles.footer}>
-      <div className={styles.title}>
-        タグ
-        </div>
-      <div>
-        <Tags tags={post.frontmatter.tags} />
       </div>
     </div>
-  </div>
+  </article>
 )
 
 export default PostLink
